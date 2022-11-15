2ab5efe0e7c03fbf3c6661a0d995aca5633a2331
DeadEye44 <josh.torrence33@gmail.com>
2022-11-14 22:44:01 -0600

    adding changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
 
+
+var message = 
+
+function countdown() {
+    var timeLeft = 180000;
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+    } else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+    } else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage();
+    }
+    }, 1000);
+}
+displayMessage();
+consoleLog.countdown = countdown();
